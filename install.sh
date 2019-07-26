fe9978c5867919a0e28a745dccf5d28563b98aee
Drew Myers <acmyers@us.ibm.com>
2019-07-26 13:35:52 -0400

    added haskeline file for vi mode in ghci

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ ln -sv ~/.dotfiles/.vimrc ~
 ln -sv ~/.dotfiles/.zshrc ~
 ln -sv ~/.dotfiles/.linux.zsh ~
 ln -sv ~/.dotfiles/.mac.zsh ~
+ln -sv ~/.dotfiles/.haskeline ~
 
 # Copy this one since it's to be locally customizable and this is just and empty template
 cp ~/.dotfiles/.local.zsh ~
