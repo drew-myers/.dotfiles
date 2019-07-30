949e9ffac496d20e8c9931ff5b9edb429a34df93
Drew Myers <acmyers@us.ibm.com>
2019-07-30 13:45:32 -0400

    tweak os-specific config

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 # !/usr/bin/env bash
+
+# Checks before running
 command -v zsh >/dev/null 2>&1 || { echo >&2 "I require zsh but it's not installed.  Aborting."; exit 1; }
 command -v curl >/dev/null 2>&1 || { echo >&2 "I require curl but it's not installed.  Aborting."; exit 1; }
+# Install oh-my-zsh if it isn't already
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-ln -sv ~/.dotfiles/.vimrc ~
-ln -sv ~/.dotfiles/.zshrc ~
-ln -sv ~/.dotfiles/.linux.zsh ~
-ln -sv ~/.dotfiles/.mac.zsh ~
-ln -sv ~/.dotfiles/.haskeline ~
-
 rm -rf ~/.oh-my-zsh/custom
 ln -sv ~/.dotfiles/oh-my-zsh-custom ~/.oh-my-zsh/custom
 
-# Copy this one since it's to be locally customizable and this is just and empty template
-cp -n ~/.dotfiles/.local.zsh ~
+# Copy generic files
+ln -sv ~/.dotfiles/.zshrc ~
+ln -sv ~/.dotfiles/.vimrc ~
+ln -sv ~/.dotfiles/.haskeline ~
 
+# OS-specific config
 if [[ "$OSTYPE" == "linux-"* ]]; then
+    echo "copying linux os-specific config"
     ln -sv ~/.dotfiles/bspwm/ ~/.config/
     ln -sv ~/.dotfiles/cava/ ~/.config
     ln -sv ~/.dotfiles/kitty/ ~/.config
@@ -24,5 +24,12 @@ if [[ "$OSTYPE" == "linux-"* ]]; then
     ln -sv ~/.dotfiles/ncmpcpp/ ~/.config
     ln -sv ~/.dotfiles/polybar/ ~/.config
     ln -sv ~/.dotfiles/qutebrowser/ ~/.config
+    ln -sv ~/.dotfiles/.linux.zsh ~
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "copying mac os-specific config"
+    ln -sv ~/.dotfiles/.mac.zsh ~
 fi
+
+# Copy this one since it's to be locally customizable and this is just and empty template
+cp -n ~/.dotfiles/.local.zsh ~
 
