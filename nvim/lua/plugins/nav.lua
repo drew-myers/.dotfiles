f38c483d3f74e60abb62a10bb3eed51f0193e484
Drew Myers <drew.myers@close.com>
2025-02-08 16:15:52 -0500

    Enable leap in visual mode

--- nvim/lua/plugins/nav.lua
+++ nvim/lua/plugins/nav.lua
@@ -3,7 +3,7 @@ return {
     'ggandor/leap.nvim',
     config = function()
       require 'leap'
-      vim.keymap.set('n', 's', '<Plug>(leap)')
+      vim.keymap.set({ 'n', 'x' }, 's', '<Plug>(leap)')
     end,
   },
 }
