bc11a24fb457c85fac1fd883cc5c1792e858e3c7
Drew Myers <drew.myers@close.com>
2025-04-15 15:16:05 -0400

    Add nushell lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -129,6 +129,9 @@ return {
         gopls = {},
         pyright = {},
         ruff = {},
+        nushell = {
+          noinstall = true,
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
